7317b00cb182fc81e761ea512a33378ad16003b2
Duane Cilliers <duanecilliers@gmail.com>
2015-05-07 13:07:53 +0200

    remove font size and padding options from button widget and add button size option

--- so-bootstrap-widgets-bundle.php
+++ so-bootstrap-widgets-bundle.php
@@ -14,7 +14,28 @@ License URI: https://www.gnu.org/licenses/gpl-3.0.txt
 class SO_Bootstrap_Widget_Bundle {
 
 	function __construct() {
+		add_filter( 'siteorigin_widgets_form_options_sow-button', array( $this, 'extend_button_form' ), 10, 2 );
 		add_filter( 'siteorigin_widgets_template_file_sow-button', array( $this, 'button_template_file' ), 10, 3 );
+	}
+
+	function extend_button_form( $form_options, $widget ) {
+		if ( isset( $form_options['design']['fields'] ) ) {
+			unset( $form_options['design']['fields']['font_size'] );
+			unset( $form_options['design']['fields']['padding'] );
+
+			$form_options['design']['fields']['size'] = array(
+				'type' => 'select',
+				'label' => __('Button Size', 'so-bootstrap-widgets-bundle'),
+				'default' => 'default',
+				'options' => array(
+					'extra_small' => __( 'Extra Small', 'so-bootstrap-widgets-bundle' ),
+					'small' => __( 'Small', 'so-bootstrap-widgets-bundle' ),
+					'default' => __( 'Default', 'so-bootstrap-widgets-bundle' ),
+					'large' => __( 'Large', 'so-bootstrap-widgets-bundle' )
+				)
+			);
+		}
+		return $form_options;
 	}
 
 	function button_template_file( $filename, $instance, $widget ) {
